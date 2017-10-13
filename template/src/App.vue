7d83d035d71c81fe00be85a792efddb8f18beb55
陈嘉涵 <chenjiahan@meituan.com>
2017-10-13 14:19:46 +0800

    update vant 0.10.0

--- template/src/App.vue
+++ template/src/App.vue
@@ -1,56 +1,30 @@
 <template>
   <div id="app">
-    <vm-preview
-      :image="image"
-      :orders="orders"
-    ></vm-preview>
-
     <van-cell-group class="item-list">
-      <van-cell isLink>
-        <van-icon name="points" class="orders-icon"></van-icon>
-        我的积分
-      </van-cell>
-      <van-cell isLink>
-        <van-icon name="gift" class="orders-icon"></van-icon>
-        我收到的礼物
-      </van-cell>
+      <van-cell isLink icon="points" title="我的积分" />
+      <van-cell isLink icon="gift" title="我收到的礼物" />
     </van-cell-group>
   </div>
 </template>
 
 <script>
 import { Cell, CellGroup, Icon } from 'vant';
-import 'vant/lib/vant-css/reset.css';
-import Preview from './components/preview.vue';
 
 export default {
   components: {
     [Cell.name]: Cell,
     [CellGroup.name]: CellGroup,
-    [Icon.name]: Icon,
-    'vm-preview': Preview
-  },
-
-  data() {
-    return {
-      image: 'https://img.yzcdn.cn/upload_files/2017/07/19/Fml_7Kkd2H6pLolssY8bqoAPwyU5.png',
-      orders: {
-        topay: 0,
-        toaccept: 0,
-        tosend: 6,
-        send: 0
-      }
-    };
+    [Icon.name]: Icon
   }
 };
 </script>
 
 <style>
-.orders-icon {
-  margin-right: 5px;
-  font-size: 20px;
+body {
+  background-color: #f8f8f8;
 }
-.item-list {
-  margin-top: 10px;
+
+.van-cell__title .van-icon {
+  font-size: 18px;
 }
 </style>
